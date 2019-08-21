b20edaa05b98c0c7bb4ba0e489d33f76fedc9151
Alexey Sheplyakov <asheplyakov@yandex.ru>
2019-08-21 23:48:22 +0400

    client: preliminary MSVC support
    Works only with clang-cl only on the remote side

--- pdistcc/compiler/__init__.py
+++ pdistcc/compiler/__init__.py
@@ -1,6 +1,7 @@
 
 import os
 from .gcc import GCCWrapper
+from .msvc import MSVCWrapper
 from .errors import UnsupportedCompiler
 
 
@@ -8,6 +9,8 @@ def wrap_compiler(host, port, compiler_cmd):
     compiler_name = os.path.basename(compiler_cmd[0])
     if compiler_name in ('gcc', 'g++'):
         wrapper = GCCWrapper(compiler_cmd)
+    elif compiler_name in ('cl', 'clang-cl', 'cl.exe', 'clang-cl.exe'):
+        wrapper = MSVCWrapper(compiler_cmd)
     else:
         raise UnsupportedCompiler(compiler_name)
     wrapper.wrap_compiler(host, port)
---
+++ pdistcc/compiler/msvc.py
@@ -0,0 +1,98 @@
+
+from .wrapper import CompilerWrapper
+from .errors import UnsupportedCompilationMode as UCM
+
+
+LANG_C = 'c'
+LANG_CXX = 'c++'
+
+
+class MSVCWrapper(CompilerWrapper):
+    def __init__(self, args):
+        super().__init__(args)
+        self._srcfile = None
+        self._objfile = None
+        self._preprocessed_file = None
+
+    def _is_source_file(self, path):
+        ext = path.split('.')[-1].lower()
+        return ext in ('c', 'cpp', 'cc', 'cxx')
+
+    def can_handle_command(self):
+        source_count = 0
+        is_object_compilation = False
+        has_object_file = False
+
+        skip_next_arg = False
+        for arg in self._args:
+            if skip_next_arg:
+                skip_next_arg = False
+                continue
+            if arg in ('/c', '/C'):
+                is_object_compilation = True
+            elif arg[0] == '@':
+                raise UCM('Response files are not supported')
+            elif arg in ('/Zi', '/ZI'):
+                raise UCM('PDB generation is not supported')
+            elif arg.startswith("/Fo"):
+                has_object_file = True
+            elif arg.startswith('/MP'):
+                raise UCM('Multiprocessing mode is unsupported')
+            elif self._is_source_file(arg):
+                self._srcfile = arg
+                source_count += 1
+
+        if source_count != 1:
+            raise UCM('Multiple source files')
+        if not (is_object_compilation and has_object_file):
+            raise UCM('Only compilation of a single source file is supported')
+
+    def _preprocessed_filename(self):
+        objname_woext = self._objfile.split('.')[:-1]
+        objname_woext.append('i')
+        self._preprocessed_file = objname_woext.join('.')
+        return self._preprocessed_file
+
+    def preprocessor_cmd(self):
+        cmd = [self._compiler]
+        for arg in self._args:
+            skip_arg = False
+            if arg in ('/c', '/E'):
+                skip_arg = True
+            elif arg.startswith('/Fo'):
+                skip_arg = True
+                self._objfile = arg[3:]
+                cmd.extend(['/P', '/Fi{}'.format(self.preprocessed_file())])
+            if not skip_arg:
+                cmd.append(arg)
+        return cmd
+
+    def _lang(self):
+        srcext = self._srcfile.split('.')[-1].lower()
+        return LANG_C if srcext == 'c' else LANG_CXX
+
+    def compiler_cmd(self):
+        cmd = [self._compiler]
+        for arg in self._args:
+            if arg == '/c':
+                # XXX: distcc
+                cmd.append('-c')
+            elif arg == self._srcfile:
+                cmd.append('/TP' if self._lang() == LANG_CXX else '/TC')
+                cmd.append(self._preprocessed_file)
+            else:
+                cmd.append(arg)
+        return cmd
+
+    def object_file(self):
+        return self._objfile
+
+    def preprocessed_file(self):
+        if self._preprocessed_file is None:
+            objname_woext = self._objfile.split('.')[:-1]
+            objname_woext.append('i')
+            self._preprocessed_file = '.'.join(objname_woext)
+        return self._preprocessed_file
+
+    def source_file(self):
+        return self._srcfile
