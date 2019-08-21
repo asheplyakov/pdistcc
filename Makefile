bfbd65079536aec1c83ab9143e0a83fa49254631
Alexey Sheplyakov <asheplyakov@yandex.ru>
2019-08-21 20:30:54 +0400

    [WIP] Implemented local preprocessing

--- Makefile
+++ Makefile
@@ -1,16 +1,13 @@
 
-all: thunderingherd dat/thunderingherd.ii thunderingherd.o
+all: thunderingherd
 
+.o/thunderingherd.o: dat/thunderingherd.cpp
+	@mkdir -p "$(dir $@)"
+	python3 ./pdistcc/pdistcc.py -- $(CXX) -std=c++11 -pthread -O2 -g -c -o $@ $<
 
-dat/thunderingherd.ii: dat/thunderingherd.cpp
-	$(CXX) -std=c++11 -pthread -E -o $@ $<
-
-thunderingherd.o: dat/thunderingherd.ii
-	python3 ./pdistcc/pdistcc.py "$<" "$@" -- /usr/bin/g++ -std=c++11 -pthread -O2 -g -c dat/thunderingherd.cpp
-
-thunderingherd: thunderingherd.o
-	/usr/bin/g++ -pthread -o $@ $^
+thunderingherd: .o/thunderingherd.o
+	$(CXX) -pthread -o $@ $^
 
 clean:
-	@rm -f thunderingherd.o
+	@rm -f .o/thunderingherd.o
 	@rm -f thunderingherd
--- pdistcc/pdistcc.py
+++ pdistcc/pdistcc.py
@@ -3,10 +3,14 @@
 import argparse
 import os
 import socket
+import subprocess
 import sys
 
 DCC_TOKEN_HEADER_LEN = 12
 DCC_VERSION = 1
+LANG_C = 'c'
+LANG_CXX = 'c++'
+
 
 def dcc_compile(doti, args, host='127.0.0.1', port=3632, ofile='a.out'):
 
@@ -101,17 +105,169 @@ def dcc_compile(doti, args, host='127.0.0.1', port=3632, ofile='a.out'):
             aout.flush()
 
 
+class UnsupportedCompiler(BaseException):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class UnsupportedCompilationMode(BaseException):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class PreprocessorFailed(BaseException):
+    pass
+
+
+class CompilerWrapper(object):
+    def __init__(self, args):
+        self._args = args[1:]
+        self._compiler = args[0]
+
+    def wrap_compiler(self, host, port):
+        self.can_handle_command()
+        preprocessor_cmd = self.preprocessor_cmd()
+        try:
+            subprocess.check_output(preprocessor_cmd)
+        except subprocess.CalledProcessError:
+            raise PreprocessorFailed()
+
+        dcc_compile(self.preprocessed_file(),
+                    self.compiler_cmd(),
+                    host=host,
+                    port=port,
+                    ofile=self.object_file())
+
+    def can_handle_command(self):
+        return False
+
+    def preprocessor_cmd(self):
+        return []
+
+    def compiler_cmd(self):
+        return []
+
+    def source_file(self):
+        pass
+
+    def object_file(self):
+        pass
+
+    def preprocessed_file(self):
+        pass
+
+
+class GCCWrapper(CompilerWrapper):
+    source_file_extensions = ('cpp', 'cxx', 'cc', 'c')
+    extension2lang = {
+        'c': LANG_C,
+    }
+
+    def __init__(self, args):
+        super().__init__(args)
+        self._srcfile = None
+        self._objfile = None
+        self._preprocessed_file = None
+
+    def _is_source_file(self, arg):
+        fileext = arg.split('.')[-1].lower()
+        return fileext in self.source_file_extensions
+
+    def _lang(self):
+        srcext = self._srcfile.split('.')[-1].lower()
+        return self.extension2lang.get(srcext, LANG_CXX)
+
+    def _preprocessed_filename(self, obj):
+        doti_suffix = 'ii' if self._lang() == LANG_CXX else 'i'
+        doti = obj.split('.')[:-1] + [doti_suffix]
+        return '.'.join(doti)
+
+    def can_handle_command(self):
+        source_count = 0
+        is_object_compilation = False
+        has_object_file = False
+
+        skip_next_arg = False
+        for arg in self._args:
+            if skip_next_arg:
+                skip_next_arg = False
+                continue
+            if arg == '-c':
+                is_object_compilation = True
+            elif self._is_source_file(arg):
+                source_count += 1
+                self._srcfile = arg
+            elif arg == '-o':
+                has_object_file = True
+                skip_next_arg = True
+
+        if source_count != 1:
+            raise UnsupportedCompilationMode('multiple sources')
+        if not is_object_compilation:
+            raise UnsupportedCompilationMode('linking')
+        if not has_object_file:
+            raise UnsupportedCompilationMode('output object not specified')
+
+    def preprocessor_cmd(self):
+        cmd = [self._compiler]
+        next_arg_is_object = False
+
+        for arg in self._args:
+            skip_arg = False
+            if '-c' == arg:
+                cmd.extend(['-E'])
+                skip_arg = True
+            elif next_arg_is_object:
+                self._objfile = arg
+                self._preprocessed_file = self._preprocessed_filename(arg)
+                cmd.append(self._preprocessed_file)
+                next_arg_is_object = False
+                skip_arg = True
+            elif '-o' == arg:
+                next_arg_is_object = True
+            else:
+                pass
+            if not skip_arg:
+                cmd.append(arg)
+        return cmd
+
+    def object_file(self):
+        return self._objfile
+
+    def preprocessed_file(self):
+        return self._preprocessed_file
+
+    def source_file(self):
+        return self._srcfile
+
+    def compiler_cmd(self):
+        cmd = [self._compiler]
+        for arg in self._args:
+            if arg != self._srcfile:
+                cmd.append(arg)
+            else:
+                cmd.append(self._preprocessed_file)
+        return cmd
+
+
+def wrap_compiler(host, port, compiler_cmd):
+    compiler_name = os.path.basename(compiler_cmd[0])
+    if compiler_name in ('gcc', 'g++'):
+        wrapper = GCCWrapper(compiler_cmd)
+    else:
+        raise UnsupportedCompiler(compiler_name)
+    wrapper.wrap_compiler(host, port)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', help='where to compile', default='127.0.0.1')
-    parser.add_argument('--port', type=int, help='port distccd listens at', default=3632)
-    parser.add_argument("ii", help="path to preprocessed source")
-    parser.add_argument("ofile", help="output file path")
+    parser.add_argument('--port', type=int,
+                        help='port distccd listens at', default=3632)
     parser.add_argument("compiler", nargs='*', help="compiler and arguments")
     args = parser.parse_args()
 
-    dcc_compile(args.ii, args.compiler, ofile=args.ofile,
-                host=args.host, port=args.port)
+    wrap_compiler(args.host, args.port, args.compiler)
 
 
 if __name__ == '__main__':
