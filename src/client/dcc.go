4dfc77dad34fb0f110f7cd4a647559212538d178
Alexey Sheplyakov <asheplyakov@yandex.ru>
2019-09-30 11:39:44 +0400

    [WIP] distcc client: implemented `request`

---
+++ src/client/dcc.go
@@ -0,0 +1,99 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"log"
+)
+
+const (
+	TOKEN_LEN      int = 12
+	TOKEN_NAME_LEN int = 4
+)
+
+type DccClient struct {
+	version int
+	rsock   io.Reader
+	wsock   io.Writer
+	stdout  io.Writer
+	stderr  io.Writer
+	doti    io.Reader
+	dotilen int
+	ofile   io.Writer
+}
+
+func DccEncode(token string, val int) string {
+	var tok [4]byte
+	copy(tok[:], token)
+	return fmt.Sprintf("%s%08x", string(tok[:]), val)
+}
+
+func DccEncodeString(token string, val string) string {
+	var tok [4]byte
+	copy(tok[:], token)
+	return fmt.Sprintf("%s%08x%s", string(tok[:]), len(val), val)
+}
+
+func sendToken(sock io.Writer, token string, val int) (err error) {
+	var n int
+	n, err = io.WriteString(sock, DccEncode(token, val))
+	if err != nil {
+		log.Println("Failed to send:", err)
+		return
+	}
+	if n != TOKEN_LEN {
+		err = fmt.Errorf("Failed to send: expected: %d bytes, actual: %d", TOKEN_LEN, n)
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func sendStringToken(sock io.Writer, token string, val string) (err error) {
+	var n int
+	encoded := DccEncodeString(token, val)
+	n, err = io.WriteString(sock, encoded)
+	if err != nil {
+		log.Println("Failed to send:", err)
+		return
+	}
+	if n != len(encoded) {
+		err = fmt.Errorf("Failed to send: expected %d bytes, actual: %d", len(encoded), n)
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func (c *DccClient) Request(args []string) (err error) {
+	var sz int64
+	err = sendToken(c.wsock, "DIST", c.version)
+	if err != nil {
+		return
+	}
+	err = sendToken(c.wsock, "ARGC", len(args))
+	if err != nil {
+		return
+	}
+	for i, arg := range args {
+		err = sendStringToken(c.wsock, "ARGV", arg)
+		if err != nil {
+			log.Println("Failed to send arg", i)
+			return
+		}
+	}
+	err = sendToken(c.wsock, "DOTI", c.dotilen)
+	if err != nil {
+		return
+	}
+	sz, err = io.Copy(c.wsock, c.doti)
+	if err != nil {
+		return
+	}
+	if sz != int64(c.dotilen) {
+		err = fmt.Errorf("Failed to send DOTI file")
+		log.Println("Failed to send DOTI file")
+		return
+	}
+	return
+}
---
+++ src/client/dcc_test.go
@@ -0,0 +1,51 @@
+package client
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestDccEncode(t *testing.T) {
+	encoded := DccEncode("OOPS", 31)
+	expected := "OOPS0000001f"
+	if encoded != expected {
+		t.Errorf("DccEncode: expected %v, actual %v", expected, encoded)
+	}
+}
+
+func TestDccEncodeString(t *testing.T) {
+	arg := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	expected := "ARGV0000001f" + arg
+	encoded := DccEncodeString("ARGV", arg)
+	if encoded != expected {
+		t.Errorf("DccEncodeString: expected %v, actual %v", expected, encoded)
+	}
+}
+
+func TestDccRequest(t *testing.T) {
+	c := new(DccClient)
+	c.version = 1
+	var wsock bytes.Buffer
+	c.wsock = &wsock
+	c.doti = bytes.NewBuffer([]byte("0xdeadbeaf"))
+	c.dotilen = 10
+	args := []string{"gcc", "-c", "-o", "foo.o", "foo.c"}
+	err := c.Request(args)
+	if err != nil {
+		t.Errorf("Unable to send command")
+	}
+	expected := []string{
+		"DIST", "00000001",
+		"ARGC", "00000005",
+		"ARGV", "00000003", "gcc",
+		"ARGV", "00000002", "-c",
+		"ARGV", "00000002", "-o",
+		"ARGV", "00000005", "foo.o",
+		"ARGV", "00000005", "foo.c",
+		"DOTI", "0000000a", "0xdeadbeaf",
+	}
+	if wsock.String() != strings.Join(expected, "") {
+		t.Errorf(`DccClient.Request: expected: "%s", actual "%s"`, expected, wsock.String())
+	}
+}
