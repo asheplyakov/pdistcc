327c648f7c535e15fd1d003bfe78803a3b9783e5
Alexey Sheplyakov <asheplyakov@altlinux.org>
2021-02-17 13:57:58 +0400

    sched: use consistent hashing for choosing a server

--- pdistcc/sched.py
+++ pdistcc/sched.py
@@ -1,15 +1,10 @@
 
-import bisect
+from uhashring import HashRing
 
 
 def pick_server(servers, key):
     if len(servers) == 1:
         return servers[0]
-    buckets = [0]
-    total_weight = 0
-    for s in servers:
-        total_weight += s['weight']
-        buckets.append(total_weight - 1)
-    bucket_idx = hash(key) % total_weight
-    server_idx = bisect.bisect_left(buckets, bucket_idx) - 1
-    return servers[server_idx]
+    # HashRing does not accept list of distcs
+    _servers = dict((n, s) for n, s in enumerate(servers))
+    return _servers[HashRing(_servers).get_node(key)]
