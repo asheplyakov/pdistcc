00f10c8c7ef1231a05de1545f3904c7885606b66
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-07-27 20:37:31 +0400

    server: collect performance metrics
    * total time of handling the request
    * compilation time
    * time to receive the (preprocessed) source
    * time to send the object file
    * the size of (preprocessed) source and object file

--- pdistcc/server.py
+++ pdistcc/server.py
@@ -4,6 +4,7 @@ import logging
 import multiprocessing
 import os
 import tempfile
+import time
 import socketserver
 import subprocess
 
@@ -30,6 +31,7 @@ class Distccd(socketserver.BaseRequestHandler):
         self._fileops = kwargs.get('fileops', FileOpsFactory())
         self._tempfile = kwargs.get('tempfile', tempfile.NamedTemporaryFile)
         self._Popen = kwargs.get('popen', subprocess.Popen)
+        self._perf = Perf()
         for arg in ('fileops', 'tempfile', 'popen'):
             if arg in kwargs:
                 del kwargs[arg]
@@ -56,6 +58,7 @@ class Distccd(socketserver.BaseRequestHandler):
         return compiler_cmd
 
     def _read_doti(self):
+        start_time = time.perf_counter()
         name, doti_bytes, _ = read_field(self.request, False)
         if name != b'DOTI':
             raise InvalidToken("expected DOTI, got {}", to_string(name))
@@ -65,6 +68,8 @@ class Distccd(socketserver.BaseRequestHandler):
             chunked_read_write(self.request, doti.file, doti_bytes)
             doti.flush()
         logger.debug('successfully read %s bytes', doti_bytes)
+        self._perf.recv_time = (time.perf_counter() - start_time)*1000
+        self._perf.recv_size = doti_bytes
         return path
 
     def _compile(self, wrapper, cleanup_files):
@@ -79,16 +84,19 @@ class Distccd(socketserver.BaseRequestHandler):
 
         compiler_cmd = wrapper.compiler_cmd()
         logger.debug('running compiler: %s', str(compiler_cmd))
+        start_time = time.perf_counter()
         compiler = self._Popen(compiler_cmd,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
         stdout, stderr = compiler.communicate()
+        self._perf.compile_time = (time.perf_counter() - start_time)*1000
         ret = compiler.returncode
         logger.debug('compiler returned: %s', ret)
         return ret, stdout, stderr, objfile
 
     def _reply(self, ret, stdout, stderr, objfile):
         logging.debug('sending reply')
+        start_time = time.perf_counter()
         buf = dcc_encode('DONE', DCC_PROTOCOL)
         buf += dcc_encode('STAT', ret)
         buf += dcc_encode('SERR', len(stderr))
@@ -104,6 +112,8 @@ class Distccd(socketserver.BaseRequestHandler):
                 logger.debug('sending object file %s', objfile)
                 chunked_send(self.request, doto, doto_len)
                 logger.debug('successfully sent %s bytes', doto_len)
+                self._perf.send_time = (time.perf_counter() - start_time)*1000
+                self._perf.send_size = doto_len
         except FileNotFoundError:
             if ret != 0:
                 self.request.sendall(dcc_encode('DOTO', 0))
@@ -114,6 +124,8 @@ class Distccd(socketserver.BaseRequestHandler):
     def handle(self):
         if 'delayed_handle' in self._settings:
             pass
+        start_time = time.perf_counter()
+        logger.info("connection from %s", self.client_address)
         cleanup_files = []
         try:
             compiler_cmd = self._read_request()
@@ -124,6 +136,8 @@ class Distccd(socketserver.BaseRequestHandler):
             wrapper.set_preprocessed_file(doti_file)
             ret, stdout, stderr, objfile = self._compile(wrapper, cleanup_files)
             self._reply(ret, stdout, stderr, objfile)
+            self._perf.total_time = (time.perf_counter() - start_time)*1000
+            logger.info("%s: request handled: %s", self.client_address, self._perf)
         except BrokenPipeError:
             # client has disconnected, ignore
             pass
@@ -131,6 +145,67 @@ class Distccd(socketserver.BaseRequestHandler):
             for p in cleanup_files:
                 if os.path.isfile(p):
                     os.remove(p)
+
+
+class Perf:
+    def __init__(self):
+        self._total_time = 0.0
+        self._compile_time = 0.0
+        self._recv_time = 0.0
+        self._send_time = 0.0
+        self._recv_size = 0
+        self._send_size = 0
+
+    @property
+    def total_time(self):
+        return self._total_time
+
+    @property
+    def recv_time(self):
+        return self._recv_time
+
+    @property
+    def compile_time(self):
+        return self._compile_time
+
+    @property
+    def send_time(self):
+        return self._send_time
+
+    @property
+    def recv_size(self):
+        return self._recv_size
+
+    @property
+    def send_size(self):
+        return self._send_size
+
+    @total_time.setter
+    def total_time(self, value):
+        self._total_time = value
+
+    @compile_time.setter
+    def compile_time(self, value):
+        self._compile_time = value
+
+    @recv_time.setter
+    def recv_time(self, value):
+        self._recv_time = value
+
+    @send_time.setter
+    def send_time(self, value):
+        self._send_time = value
+
+    @recv_size.setter
+    def recv_size(self, value):
+        self._recv_size = value
+
+    @send_size.setter
+    def send_size(self, value):
+        self._send_size = value
+
+    def __str__(self):
+        return f'total: {self._total_time:.2f}, compile: {self._compile_time:.2f}, recv: {self._recv_time:.2f}, send: {self._send_time:.2f}, recv size: {self._recv_size}, send size: {self._send_size}'
 
 
 def daemon(settings, host='127.0.0.1', port=3632):
