e6375355de7cfdf2cb6df1d6df6522c4d59f998d
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-07-27 20:37:31 +0400

    server: collect performance metrics
    * total time of handling the request
    * compilation time
    * time to receive the (preprocessed) source
    * time to send the object file
    * the size of (preprocessed) source and object file

--- pdistcc/server.py
+++ pdistcc/server.py
@@ -47,6 +47,7 @@ class Distccd:
         self._fileops = AsyncFileOpsFactory()
         self._Popen = _popen
         self._client_address = None
+        self._perf = Perf()
 
     async def _read_compiler_cmd(self, argc):
         compiler_cmd = []
@@ -70,6 +71,7 @@ class Distccd:
 
     async def _read_doti(self):
         name, doti_bytes, _ = await read_field(self._reader, False)
+        ti = time.perf_counter()
         if name != b'DOTI':
             raise InvalidToken("expected DOTI, got {}", to_string(name))
         logger.debug(f'reading doti file from {self._client_address}')
@@ -84,6 +86,8 @@ class Distccd:
                 await doti.write(chunk)
                 size = size - len(chunk)
             await doti.flush()
+        self._perf.recv_time = (time.perf_counter() - ti)*1000
+        self._perf.recv_size = doti_bytes
         logger.debug('successfully read %s bytes', doti_bytes)
         return path
 
@@ -95,13 +99,16 @@ class Distccd:
 
         compiler_cmd = wrapper.compiler_cmd()
         logger.debug('running compiler: %s', str(compiler_cmd))
+        ti = time.perf_counter()
         compiler = await self._Popen(compiler_cmd)
         stdout, stderr = await compiler.communicate()
+        self._perf.compile_time = (time.perf_counter() - ti)*1000
         ret = compiler.returncode
         logger.debug('compiler returned: %s', ret)
         return ret, stdout, stderr, objfile
 
     async def _reply(self, ret, stdout, stderr, objfile):
+        ti = time.perf_counter()
         logging.debug(f'sending reply to {self._client_address}')
         self._writer.write(dcc_encode('DONE', DCC_PROTOCOL))
         self._writer.write(dcc_encode('STAT', ret))
@@ -124,6 +131,8 @@ class Distccd:
                     self._writer.write(chunk)
                     size = size - len(chunk)
                 await self._writer.drain()
+                self._perf.send_time = (time.perf_counter() - ti)*1000
+                self._perf.send_size = doto_len
                 logger.debug('successfully sent %s bytes', doto_len)
         except FileNotFoundError:
             if ret != 0:
@@ -149,9 +158,8 @@ class Distccd:
             await self._writer.drain()
             self._writer.close()
             await self._writer.wait_closed()
-            elapsed_time = (time.perf_counter() - start_time)*1000
-            logger.info(f'request handled in {elapsed_time:.3f} msec')
-            logger.info(f'closed connection with {self._client_address!r}')
+            self._perf.total_time = (time.perf_counter() - start_time)*1000
+            logger.info(f'{self._client_address!r} request handled: {self._perf}')
         except BrokenPipeError:
             # client has disconnected, ignore
             logger.info('client "%s": premature disconnect', self._client_address)
@@ -169,8 +177,68 @@ class Distccd:
                     pass
 
 
-async def _daemon(settings, host='127.0.0.1', port=3632):
+class Perf:
+    def __init__(self):
+        self._total_time = 0.0
+        self._compile_time = 0.0
+        self._recv_time = 0.0
+        self._send_time = 0.0
+        self._recv_size = 0
+        self._send_size = 0
 
+    @property
+    def total_time(self):
+        return self._total_time
+
+    @property
+    def recv_time(self):
+        return self._recv_time
+
+    @property
+    def compile_time(self):
+        return self._compile_time
+
+    @property
+    def send_time(self):
+        return self._send_time
+
+    @property
+    def recv_size(self):
+        return self._recv_size
+
+    @property
+    def send_size(self):
+        return self._send_size
+
+    @total_time.setter
+    def total_time(self, value):
+        self._total_time = value
+
+    @compile_time.setter
+    def compile_time(self, value):
+        self._compile_time = value
+
+    @recv_time.setter
+    def recv_time(self, value):
+        self._recv_time = value
+
+    @send_time.setter
+    def send_time(self, value):
+        self._send_time = value
+
+    @recv_size.setter
+    def recv_size(self, value):
+        self._recv_size = value
+
+    @send_size.setter
+    def send_size(self, value):
+        self._send_size = value
+
+    def __str__(self):
+        return f'total: {self._total_time:.2f}, compile: {self._compile_time:.2f}, recv: {self._recv_time:.2f}, send: {self._send_time:.2f}, recv size: {self._recv_size}, send size: {self._send_size}'  
+
+
+async def _daemon(settings, host='127.0.0.1', port=3632):
     async def dcc_handler(reader, writer):
         handler = Distccd(reader, writer, settings)
         await handler.run()
